58aef782ffc36e7208de312dc04e63ce73b32282
Emily McCray <emilyrmccray@gmail.com>
2024-09-30 22:04:08 -0400

    uploading previously completed week 3 case study to git

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,42 @@
- 
+library(ggplot2)
+library(gapminder)
+library(dplyr)
+
+head(gapminder)
+
+filtered <- filter(gapminder, country != "Kuwait")
+
+View(filtered)
+
+#plot 1
+plot_1 <- ggplot(filtered, aes(lifeExp, gdpPercap)) +
+  geom_point(aes(col = continent, size = pop/100000, group = country)) +
+  facet_wrap(~year, nrow = 1) +
+  scale_y_continuous(trans = "sqrt") +
+  theme_bw() +
+  labs(x = "Life Expectency", 
+       y = "GDP per capita", 
+       title = "Wealth and life expectancy through time",
+       color = "Continent", 
+       size = "Population (100k)")
+
+#plot 2
+gapminder_continent <- filtered %>%
+  group_by(continent, year) %>%
+  summarize(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
+            pop = sum(as.numeric(pop)))
+
+
+plot_2 <- ggplot() +
+  geom_line(filtered, mapping = aes(year, gdpPercap, col = continent)) +
+  geom_point(filtered, mapping = aes(year, gdpPercap, col = continent)) +
+  labs(color = "Continent") +
+  geom_line(gapminder_continent, mapping = aes(year, gdpPercapweighted)) +
+  geom_point(gapminder_continent, mapping = aes(year, gdpPercapweighted, size = pop/100000)) +
+  labs(size = "Population (100k)") +
+  facet_wrap(~continent, nrow = 1) +
+  scale_y_continuous(trans = "sqrt") +
+  theme_bw()
+labs(x = "Year", 
+     y = "GDP per capita", 
+     title = "Wealth and life expectancy through time") 
